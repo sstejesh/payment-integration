1b2ff6c5edc8dcbde9bfb1cfd66c98eca3bdbf58
sstejesh <63189490+sstejesh@users.noreply.github.com>
2020-08-15 19:28:27 +0530

    payment.php

---
+++ payment.php
@@ -0,0 +1,36 @@
+
+<?php
+
+$Name=$_POST['Name'];
+$Email=$_POST['Email'];
+$Amount=$_POST['Amount'];
+$Phone=$_POST['phone'];
+$purpose='Donation';
+
+
+include 'instamojo/Instamojo.php';
+$api = new Instamojo\Instamojo('test_785179e8fc420d0f7c1bb358384', 'test_c7bd3b894841458f8166a9cf432', 'https://test.instamojo.com/api/1.1/');
+
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => $purpose,
+        "amount" => $Amount,
+        "send_email" => true,
+        "email" => $Email,
+        "buyer_name" =>$Name,
+        "phone"=>$Phone,
+        "send_sms" => true,
+        "allow_repeated_payments" =>false,
+        "redirect_url" => "https://payment-gateway-integration.000webhostapp.com/redirect.php"
+        ));
+    //print_r($response);
+    $pay_url=$response['longurl'];
+    header("location: $pay_url");
+	}
+	catch (Exception $e) {
+	    print('Error: ' . $e->getMessage());
+	}
+
+
+
+?>
