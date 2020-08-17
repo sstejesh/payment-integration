60b02d6b43dfdb1026f28f238f8492781240b2db
sstejesh <63189490+sstejesh@users.noreply.github.com>
2020-08-17 16:04:56 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -4,41 +4,35 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
+    <title>Thank You</title>
 
-    <title>Thank You For Donation</title>
-
-<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
 
-  <body>
+  <body style= "background-color: #02faef">
     <div class="container">
 
       <div class="page-header">
         
-        <p class="lead" style=color:red>Thank you for your donation to Orphan Rescue.</p>
+        <p class="lead" style=color:black>Thank you for your donation to Orphan Rescue.</p>
       </div>
 
-      <strong><h3 style="color:#6da552">Thank You..!! Payment Done Sucessfully!!</h3> </strong>
+      <strong><h3 style="color:#0c7b03">Payment Done Sucessfully!!</h3> </strong>
    
-    <p> Thank you for supporting Charity work with your generous regular donations. Your valuable gift is helping us provide long-term support to girls and poor people.
-</p>
-<p>Note : Check Your Email For Transection Detail</p>
+    <p> Thank you for supporting with your valuable donation. Your valuable donation is helpful for our children. </p>
+<p>Please Check Your Email For Transaction Details</p>
  <?php
 
 include 'instamojo.php';
 
-$api = new Instamojo\Instamojo('test_3743a7a31d4c8d710f488ba8e39', 'test_2df6effdc4b9b2e6dd53dddd5a1','https://test.instamojo.com/api/1.1/');
+$api = new Instamojo\Instamojo('test_785179e8fc420d0f7c1bb358384', 'test_c7bd3b894841458f8166a9cf432','https://test.instamojo.com/api/1.1/');
 
 $payid = $_GET["payment_request_id"];
 
@@ -48,16 +42,13 @@ try {
 
 
     echo "<h4>Payment ID: " . $response['payments'][0]['payment_id'] . "</h4>" ;
-    echo "<h4>Payment Email: " . $response['payments'][0]['amount'] . "</h4>";
     echo "<h4>Payment Name: " . $response['payments'][0]['buyer_name'] . "</h4>" ;
     echo "<h4>Payment Email: " . $response['payments'][0]['buyer_email'] . "</h4>" ;
+    echo "<h4>Amount Paid: " . $response['payments'][0]['amount'] . "</h4>";
+
     
 
-  echo "<pre>";
-   print_r($response);
-echo "</pre>";
     ?>
-
 
     <?php
 }
@@ -65,15 +56,12 @@ catch (Exception $e) {
     print('Error: ' . $e->getMessage());
 }
 
-
-
   ?>
 
-
+<h4>To go back to our website, <a href="
+https://phoebean-cables.000webhostapp.com/task4.html">Click Here</a></h4>
       
     </div> <!-- /container -->
-
-
 
 
   </body>
