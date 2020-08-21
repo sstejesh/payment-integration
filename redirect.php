ce09847d4091267ed7b1d8b2afe58cde2f8bdf58
sstejesh <63189490+sstejesh@users.noreply.github.com>
2020-08-21 20:38:06 +0530

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -4,19 +4,24 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
+
     <title>Thank You</title>
 
+<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+<!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
+<!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
 
-  <body style= "background-color: #02faef">
+  <body style= "background-color: #bdc3c7">
     <div class="container">
 
       <div class="page-header">
@@ -24,7 +29,7 @@
         <p class="lead" style=color:black>Thank you for your donation to Orphan Rescue.</p>
       </div>
 
-      <strong><h3 style="color:#0c7b03">Payment Done Sucessfully!!</h3> </strong>
+      <strong><h3 style="color:#239B56">Payment Done Successfully!!</h3> </strong>
    
     <p> Thank you for supporting with your valuable donation. Your valuable donation is helpful for our children. </p>
 <p>Please Check Your Email For Transaction Details</p>
@@ -40,6 +45,7 @@ $payid = $_GET["payment_request_id"];
 try {
     $response = $api->paymentRequestStatus($payid);
 
+
     echo "<h4>Payment ID: " . $response['payments'][0]['payment_id'] . "</h4>" ;
     echo "<h4>Payment Name: " . $response['payments'][0]['buyer_name'] . "</h4>" ;
     echo "<h4>Payment Email: " . $response['payments'][0]['buyer_email'] . "</h4>" ;
@@ -47,7 +53,11 @@ try {
 
     
 
+  //echo "<pre>";
+   //print_r($response);
+ //echo "</pre>";
     ?>
+
 
     <?php
 }
@@ -55,11 +65,16 @@ catch (Exception $e) {
     print('Error: ' . $e->getMessage());
 }
 
+
+
   ?>
 
-<h4>To go back to our website, <a href="https://phoebean-cables.000webhostapp.com">Click Here</a></h4>
+<h4>To go back to our website, <a href="
+https://phoebean-cables.000webhostapp.com">Click Here</a></h4>
       
-    </div>
+    </div> <!-- /container -->
+
+
 
 
   </body>
